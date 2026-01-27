8785cf95f7f02ac95ee89c2456d86e95d17c3ab3
Ilya Bychkov <i.bychkov.i8@gmail.com>
2026-01-27 19:49:39 +0300

    Basic CSVReader with tests

---
+++ src/csv/csv_reader.cpp
@@ -0,0 +1,63 @@
+#include "csv_reader.h"
+
+CSVReader::CSVReader(const std::string& filename) : filename_(filename) {
+}
+
+CSVReader::~CSVReader() {
+    if (fin_.is_open()) {
+        fin_.close();
+    }
+}
+
+bool CSVReader::Open() {
+    fin_.open(filename_);
+    return fin_.is_open();
+}
+
+bool CSVReader::HasNext() {
+    return fin_.is_open() && !crashed_ && !fin_.eof();
+}
+
+std::expected<std::vector<std::string>, std::string> CSVReader::NextStr() {
+    if (crashed_) {
+        return std::unexpected("reader is crashed");
+    }
+
+    if (!fin_.is_open()) {
+        return std::unexpected("file is not open");
+    }
+
+    std::vector<std::string> fields;
+    std::string fld;
+    bool in_quotes = false;
+    char c;
+
+    while (fin_.get(c)) {
+        if (c == '"') {
+            if (in_quotes && fin_.peek() == '"') {
+                fin_.get();
+                fld += '"';
+            } else {
+                in_quotes = !in_quotes;
+            }
+        } else if (c == ',' && !in_quotes) {
+            fields.push_back(fld);
+            fld.clear();
+        } else if (c == '\n' && !in_quotes) {
+            fields.push_back(fld);
+            return fields;
+        } else {
+            fld += c;
+        }
+    }
+
+    if (!fld.empty() || !fields.empty()) {
+        if (!fld.empty()) {
+            fields.push_back(fld);
+        }
+        return fields;
+    }
+
+    crashed_ = true;
+    return std::unexpected("no more records");
+}
