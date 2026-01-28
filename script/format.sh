4fee730ca239cf694a6b72dbca86157c668f217d
Ilya Bychkov <i.bychkov.i8@gmail.com>
2026-01-28 20:44:07 +0300

    Ny pzh 13

--- script/format.sh
+++ script/format.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-# This script must be called from the root folder of the repository
 find src/ include/ tests/ -type f \( -name '*.c' -o -name '*.h' -o -name '*.cpp' -o -name '*.cc' \) -print0 | xargs -0 clang-format --dry-run -Werror
