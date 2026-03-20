1ca1d8b430b4134f65b3b1fc044d1552533f60f6
Ilya Bychkov <iibychkov@edu.hse.ru>
2026-03-20 16:50:00 +0300

    hits_sample.br constructed

--- tests/transform/csv_to_columnar_test.cpp
+++ tests/transform/csv_to_columnar_test.cpp
@@ -4,9 +4,9 @@
 
 int main() {
     CSVToColumnarTransformer transformer(
-        "/home/ilya-bychkov/VsCodeProjects/BricksDB/test_files/data.csv",
-        "/home/ilya-bychkov/VsCodeProjects/BricksDB/test_files/scheme.csv",
-        "/home/ilya-bychkov/VsCodeProjects/BricksDB/test_files/data.br", 1);
+        "/home/ilya-bychkov/VsCodeProjects/BricksDB/hits_files/hits_sample.csv",
+        "/home/ilya-bychkov/VsCodeProjects/BricksDB/hits_files/scheme.csv",
+        "/home/ilya-bychkov/VsCodeProjects/BricksDB/hits_files/hits_sample.br", 1);
     auto res = transformer.Transform();
     if (!res) {
         std::cout << "Transformation failed: " << res.error() << std::endl;
